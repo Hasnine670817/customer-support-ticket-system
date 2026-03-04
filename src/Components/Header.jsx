ffad3d8bf7b431f3ca2e0e34e84b3d13ab8872f0
Hasnine Torun <hasninetorun@gmail.com>
2026-03-04 14:52:54 +0600

    project-setup done

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header className="bg-white border border-b-[#E9E9E9]">
+            <div className="container-custom">
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import MainLayouts from "../Layouts/MainLayouts";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayouts></MainLayouts>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+        ]
+    },
+])
+
+export default router;
